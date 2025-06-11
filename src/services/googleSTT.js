b702cf27f792331218ba5f7a3d6ca4c08986ec86
alaa adel <alaaadeln7@gmail.com>
2025-06-11 07:03:07 +0300

    change the import way in cerdantial in stt google file

--- src/services/googleSTT.js
+++ src/services/googleSTT.js
@@ -4,8 +4,7 @@ config();
 const client = new speech.SpeechClient({
   credentials: {
     client_email: process.env.CLIENT_GOOGLE_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY_STT.replace(/\\n/g, "\n"),
+    private_key: process.env.GOOGLE_PRIVATE_KEY_STT,
   },
 });
-
 export default client;
